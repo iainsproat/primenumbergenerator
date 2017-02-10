25d1d92f05884dd3bcc50ee9e3216b76f4c0160f
Iain Sproat <iainsproat@gmail.com>
2017-02-10 22:41:01 +0400

    Hello World! command line

---
+++ src/src/com/iainsproat/simscaletest/primenumbergenerator/commandline/CommandLineUI.java
@@ -0,0 +1,16 @@
+package com.iainsproat.simscaletest.primenumbergenerator.commandline;
+
+import com.iainsproat.simscaletest.primenumbergenerator.core.*;
+
+/**
+ * This class is responsible for reading and writing to the command line
+ * @author Iain Sproat
+ *
+ */
+public class CommandLineUI {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
