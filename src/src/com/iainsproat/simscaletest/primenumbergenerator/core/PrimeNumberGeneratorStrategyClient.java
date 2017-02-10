6a775a46f1ce8f59ae134902148d0870789a3878
Iain Sproat <iainsproat@gmail.com>
2017-02-11 01:47:18 +0400

    Resolves #7
    Client runs strategy and times execution.
    Basic unit tests provided.

---
+++ src/src/com/iainsproat/simscaletest/primenumbergenerator/core/PrimeNumberGeneratorStrategyClient.java
@@ -0,0 +1,90 @@
+package com.iainsproat.simscaletest.primenumbergenerator.core;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * Responsible for running the Prime Number Generator strategy
+ *
+ */
+public class PrimeNumberGeneratorStrategyClient {
+	
+	/**
+	 * 
+	 * @param requestedStrategy
+	 * @param lowerBound
+	 * @param upperBound
+	 * @return
+	 */
+	public Result execute(String requestedStrategy, int lowerBound, int upperBound)
+	{
+		PrimeNumberGeneratorStrategy strategy = StrategySelector.selectStrategy(requestedStrategy);
+		return execute(strategy, lowerBound, upperBound);
+	}
+	
+	/**
+	 * Overloaded operator to allow for test injection of Mock strategy
+	 */
+	public Result execute(PrimeNumberGeneratorStrategy strategy, int lowerBound, int upperBound)
+	{		
+		Instant start = Instant.now();
+		List<Integer> primes = strategy.Execute(lowerBound, upperBound);		
+		Instant end = Instant.now();
+		
+		return new Result(start.getNano(), strategy.getClass().getSimpleName(), lowerBound, upperBound, primes, Duration.between(start, end).getNano());
+	}
+	
+	/**
+	 * Holds data pertaining to the input parameters and results of a single run of a Prime Number Generator
+	 *
+	 */
+	public class Result{
+		private int _timeStamp; //FIXME this should be a GUID as timestamps are not guaranteed to be universally unique (despite it being in nanoseconds!).
+		private String _strategy;
+		private int _lowerBound;
+		private int _upperBound;
+		private List<Integer> _primes;
+		private int _duration;
+		
+		public Result(int timeStamp, String requestedStrategy, int lowerBound, int upperBound, List<Integer> primeNumbers, int executionDuration)
+		{
+			this._timeStamp = timeStamp;
+			this._strategy = requestedStrategy;
+			this._lowerBound = lowerBound;
+			this._upperBound = upperBound;
+			this._primes = primeNumbers;
+			this._duration = executionDuration;
+		}
+		
+		public int getTimeStamp()
+		{
+			return this._timeStamp;
+		}
+		
+		public String getRequestedStrategy()
+		{
+			return this._strategy;
+		}
+		
+		public int getLowerBound()
+		{
+			return this._lowerBound;
+		}
+		
+		public int getUpperBound()
+		{
+			return this._upperBound;
+		}
+		
+		public List<Integer> getPrimeNumbers()
+		{
+			return this._primes;
+		}
+		
+		public int getExecutionDuration()
+		{
+			return this._duration;
+		}
+	}
+}
---
+++ src/test/com/iainsproat/simscaletest/primenumbergenerator/core/test/PrimeNumberGeneratorStrategyClientTest.java
@@ -0,0 +1,42 @@
+package com.iainsproat.simscaletest.primenumbergenerator.core.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.iainsproat.simscaletest.primenumbergenerator.core.PrimeNumberGeneratorStrategy;
+import com.iainsproat.simscaletest.primenumbergenerator.core.PrimeNumberGeneratorStrategyClient;
+
+public class PrimeNumberGeneratorStrategyClientTest {
+
+	@Test
+	public void testExecutePrimeNumberGeneratorStrategyIntInt() {
+		PrimeNumberGeneratorStrategyClient SUT = new PrimeNumberGeneratorStrategyClient();
+		PrimeNumberGeneratorStrategyClient.Result result = SUT.execute(new MockPrimeNumberGeneratorStrategy(), 0, 10);
+		assertEquals(result.getPrimeNumbers().size(), 3);
+		assertEquals((int)result.getPrimeNumbers().get(1), 5);
+		assertTrue(450000000 <= result.getExecutionDuration() && result.getExecutionDuration() <= 550000000); //duration is approximately near 500 milliseconds (numbers in nanoseconds)
+	}
+	
+	public class MockPrimeNumberGeneratorStrategy implements PrimeNumberGeneratorStrategy
+	{
+		@Override
+		public List<Integer> Execute(int lowerBound, int upperBound) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				fail(e.getMessage());
+			}
+			
+			List<Integer> result = new ArrayList<Integer>();
+			result.add(3);
+			result.add(5);
+			result.add(7);
+			return result;
+		}
+		
+	}
+}
