f5501870e27f04b082cd71d137a7a6794f4b519d
Iain Sproat <iainsproat@gmail.com>
2017-02-11 01:03:30 +0400

    Renaming CommandLineUI to CommandLineInterface

--- src/src/com/iainsproat/simscaletest/primenumbergenerator/commandline/CommandLineInterface.java
+++ src/src/com/iainsproat/simscaletest/primenumbergenerator/commandline/CommandLineInterface.java
@@ -1,16 +1,15 @@
 package com.iainsproat.simscaletest.primenumbergenerator.commandline;
 
-import com.iainsproat.simscaletest.primenumbergenerator.core.*;
+import com.iainsproat.simscaletest.primenumbergenerator.core.PrimeNumberGeneratorStrategyClient;
 
 /**
  * This class is responsible for reading and writing to the command line
  * @author Iain Sproat
  *
  */
-public class CommandLineUI {
+public class CommandLineInterface {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
-
 }
