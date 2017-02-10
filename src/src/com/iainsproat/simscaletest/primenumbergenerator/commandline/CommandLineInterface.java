ee92f47da178a989eb24279057ae9b6d33d91cc2
Iain Sproat <iainsproat@gmail.com>
2017-02-11 03:44:00 +0400

    command line interface provided
    Fixes #4
    There may be a problem in that JCommander isn't available in the typical
    Maven repositories; the CI may not have access to it.

--- src/src/com/iainsproat/simscaletest/primenumbergenerator/commandline/CommandLineInterface.java
+++ src/src/com/iainsproat/simscaletest/primenumbergenerator/commandline/CommandLineInterface.java
@@ -1,15 +1,27 @@
 package com.iainsproat.simscaletest.primenumbergenerator.commandline;
 
+import com.beust.jcommander.JCommander;
+
 import com.iainsproat.simscaletest.primenumbergenerator.core.PrimeNumberGeneratorStrategyClient;
 
 /**
  * This class is responsible for reading and writing to the command line
- * @author Iain Sproat
  *
  */
 public class CommandLineInterface {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		CommandLineParameters parameters = new CommandLineParameters();
+		new JCommander(parameters, args); //parsing the command line is delegated to the JCommander library
+		PrimeNumberGeneratorStrategyClient client = new PrimeNumberGeneratorStrategyClient();
+		
+		PrimeNumberGeneratorStrategyClient.Result result = client.execute(parameters.strategy, parameters.lowerBound.intValue(), parameters.upperBound.intValue());
+		
+		System.out.println(String.format("You requested a prime numbers between %s and %s calculated using the %s strategy", result.getLowerBound(), result.getUpperBound(), result.getRequestedStrategy()));
+		System.out.println(String.format("The program calculated the following results in %s seconds:", result.getExecutionDuration()/1000000000));
+		for(Integer i : result.getPrimeNumbers())
+		{
+			System.out.print(String.format("%s, ", i));
+		}
 	}
 }
