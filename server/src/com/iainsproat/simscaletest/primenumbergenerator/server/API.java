ccefd55b7402dca141b467aa7d8b6de91d23302c
Iain Sproat <iainsproat@gmail.com>
2017-02-11 17:03:33 +0400

    Simple GET REST endpoint
    Parses request and returns plain text

--- server/src/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
+++ server/src/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
@@ -4,6 +4,11 @@ import static spark.Spark.*;
 
 public class API {
 	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello World");
+		get("/primes/:strategy", (req, res) -> {
+			return String.format("You requested Prime Numbers calculated by the %s strategy, between %d and %d",
+					req.params(":strategy"), 
+					req.queryMap("lower").integerValue(), 
+					req.queryMap("upper").integerValue()); 
+		});
 	}
 }
