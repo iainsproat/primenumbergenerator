0d775aa63f92b8ceb1ef8788ae266756d9c0d320
Iain Sproat <iainsproat@gmail.com>
2017-02-18 02:13:23 +0400

    Exclude javax.servlet
    Javax.servlet is excluded from Spark dependency
    Travis starts TomCat on port 4567
    API now closes and releases resources correctly

--- server/src/main/java/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
+++ server/src/main/java/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
@@ -1,5 +1,8 @@
 package com.iainsproat.simscaletest.primenumbergenerator.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.iainsproat.simscaletest.primenumbergenerator.server.controller.PrimeNumberGeneratorController;
 
 import spark.Spark;
@@ -13,13 +16,26 @@ import spark.servlet.SparkApplication;
  * See http://sparkjava.com/documentation.html for documentation of this framework. 
  */
 public class API implements SparkApplication {
+	private Logger logger = LoggerFactory.getLogger(API.class);
+	
+	PrimeNumberGeneratorController controller;
 	
 	@Override
 	public void init() {
-		new PrimeNumberGeneratorController();
+		controller = new PrimeNumberGeneratorController();
 
 		Spark.exception(Exception.class, (exception, request, response) -> {
 			exception.printStackTrace();
 		});
+	}
+	
+	@Override
+	public void destroy() {
+		try {
+			spark.Spark.stop(); //stop the server
+			controller.close(); //close the database connection
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
 	}
 }
