ad62f3ef879af7032590f42fd18f5fe979927185
Iain Sproat <iainsproat@gmail.com>
2017-02-18 00:33:26 +0400

    Added WEB-INF
    web.xml added
    Removed redundant db resources folder
    Change API to use SparkApplication
    Removed shade and jar plugin references, and added war packaging element
    Travis CI to run tomcat for testing

--- server/src/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
+++ server/src/com/iainsproat/simscaletest/primenumbergenerator/server/API.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import com.iainsproat.simscaletest.primenumbergenerator.server.controller.PrimeNumberGeneratorController;
 
 import spark.Spark;
+import spark.servlet.SparkApplication;
 
 /**
  * Responsible for defining routes of the REST API
@@ -15,28 +16,14 @@ import spark.Spark;
  *
  * See http://sparkjava.com/documentation.html for documentation of this framework. 
  */
-public class API {
+public class API implements SparkApplication {
 	
-	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(API.class);
-		
-		PrimeNumberGeneratorController controller = new PrimeNumberGeneratorController();
-		 
-		 Spark.exception(Exception.class, (exception, request, response) -> {
-			    exception.printStackTrace();
-			});
-		 
-		 //FIXME I don't think this code is ever reached...
-		 //stop the web service
-		 try{
-			 logger.info("Closing web service");
-			 Spark.stop();
-			 controller.close();
-		 }
-		 catch(Exception e)
-		 {
-			 logger.error(e.getMessage());
-			 logger.error(e.getStackTrace().toString());
-		 }
+	@Override
+	public void init() {
+		new PrimeNumberGeneratorController();
+
+		Spark.exception(Exception.class, (exception, request, response) -> {
+			exception.printStackTrace();
+		});
 	}
 }
